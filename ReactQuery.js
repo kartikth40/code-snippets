138c584ba0aeec9c275548c64919fb0d6133e2e7
Kartik Thakur <kartikthakur.2409@gmail.com>
2023-04-27 12:29:21 +0530

    Create ReactQuery.js

---
+++ ReactQuery.js
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query'
+
+//
+// useQuery 
+function Todos() {
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodoList,
+  })
+
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
+
+  // We can assume by this point that `isSuccess === true`
+  return (
+    <ul>
+      {data.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  )
+}
+//
+//
