0675f1f17a45399d6cb95ddf80e06da520b04647
Kartik Thakur <kartikthakur.2409@gmail.com>
2023-05-10 15:08:14 +0530

    Update ReactQuery.js

--- ReactQuery.js
+++ ReactQuery.js
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useMutation } from '@tanstack/react-query'
 
 //
 // useQuery 
@@ -26,4 +26,34 @@ function Todos() {
   )
 }
 //
-//
+// useMutation
+function App() {
+  const mutation = useMutation(newTodo => {
+    return axios.post('/todos', newTodo)
+  })
+
+  return (
+    <div>
+      {mutation.isLoading ? (
+        'Adding todo...'
+      ) : (
+        <>
+          {mutation.isError ? (
+            <div>An error occurred: {mutation.error.message}</div>
+          ) : null}
+
+          {mutation.isSuccess ? <div>Todo added!</div> : null}
+
+          <button
+            onClick={() => {
+              mutation.mutate({ id: new Date(), title: 'Do Laundry' })
+            }}
+          >
+            Create Todo
+          </button>
+        </>
+      )}
+    </div>
+  )
+}
+
