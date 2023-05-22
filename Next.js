1c8da871906bec231283e2aaafb2a6b5680169b7
Kartik Thakur <kartikthakur.2409@gmail.com>
2023-05-22 21:48:19 +0530

    Update and rename NEXT.JS to Next.js

---
+++ Next.js
@@ -0,0 +1,14 @@
+// data rendering options
+export const dynamic = 'auto'  // 'auto' (default) | 'force-dynamic' | 'force-static'
+export const revalidate = 3600 // revalidate data after certain no. of seconds
+
+// data fetching using axios
+//
+
+// generateStaticParams
+// used when data do not change very often (ideal for blog posts), should be used with revalidate 
+export async function generateStaticParams() {
+  const posts = await fetch('endpoint_url').then(res => res.json())
+  return posts.map(post => ({slug: post.slug}))  // unique identifiers as slugs
+}
+
