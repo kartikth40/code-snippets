571580a642cef4d82b33c9d3f43f17463e06d2c7
Kartik Thakur <kartikthakur.2409@gmail.com>
2023-08-05 13:19:29 +0530

    Update Next.js

--- Next.js
+++ Next.js
@@ -17,3 +17,26 @@ export async function generateStaticParams() {
   return posts.map(post => ({slug: post.slug}))  // unique identifiers as slugs
 }
 
+
+
+// useNavigationEvent.tsx
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect, useRef } from "react";
+
+export const useNavigationEvent = (onPathnameChange: () => void) => {
+  const pathname = usePathname(); // Get current route
+
+  // Save pathname on component mount into a REF
+  const savedPathNameRef = useRef(pathname);
+
+  useEffect(() => {
+    // If REF has been changed, do the stuff
+    if (savedPathNameRef.current !== pathname) {
+      onPathnameChange();
+      // Update REF
+      savedPathNameRef.current = pathname;
+    }
+  }, [pathname, onPathnameChange]);
+};
